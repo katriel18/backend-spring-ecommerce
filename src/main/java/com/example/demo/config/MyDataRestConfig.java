d4ceb4d57dc952f20de0df10d97266b012833467
katriel <katriel181920@gmail.com>
2020-05-25 23:21:08 -0500

    version 1.10

--- src/main/java/com/example/demo/config/MyDataRestConfig.java
+++ src/main/java/com/example/demo/config/MyDataRestConfig.java
@@ -1,15 +1,15 @@
 package com.example.demo.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Configuration;/*
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;*/
+//import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+/*import org.springframework.http.HttpMethod;
 
 import com.example.demo.entiy.Product;
 import com.example.demo.entiy.ProductCategory;
-
+*/
 @Configuration
-public class MyDataRestConfig implements RepositoryRestConfigurer {
+public class MyDataRestConfig/* implements RepositoryRestConfigurer*/{
 /*
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
--- src/main/java/com/example/demo/dao/ProductRepository.java
+++ src/main/java/com/example/demo/dao/ProductRepository.java
@@ -1,11 +1,16 @@
 package com.example.demo.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entiy.Product;
 
 @CrossOrigin(origins = "http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
+	Page<Product> findByCategoryId(@RequestParam("id") Long id,Pageable pageable);
+	
 }
