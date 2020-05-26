d8f24bbce34d1b7ab6308de9afc96b4d135e79c5
katriel <katriel181920@gmail.com>
2020-05-26 13:11:45 -0500

    version 1.12

--- src/main/java/com/example/demo/config/MyDataRestConfig.java
+++ src/main/java/com/example/demo/config/MyDataRestConfig.java
@@ -1,16 +1,33 @@
 package com.example.demo.config;
 
-import org.springframework.context.annotation.Configuration;/*
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;*/
-//import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-/*import org.springframework.http.HttpMethod;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
 
 import com.example.demo.entiy.Product;
 import com.example.demo.entiy.ProductCategory;
-*/
+
 @Configuration
-public class MyDataRestConfig/* implements RepositoryRestConfigurer*/{
-/*
+public class MyDataRestConfig implements RepositoryRestConfigurer{
+	
+	private EntityManager entityManager;
+	
+	@Autowired
+	public MyDataRestConfig(EntityManager entityManager) {
+		
+		this.entityManager = entityManager;
+	}
+	
+
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 
@@ -25,7 +42,35 @@ public class MyDataRestConfig/* implements RepositoryRestConfigurer*/{
 		config.getExposureConfiguration().forDomainType(ProductCategory.class)
 				.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuportActions))
 				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuportActions));
+		
+		//LLAMANDO AL METODO QUE EXPONGA LAS IDS
+		exposeIds(config);
+	}
 
-	}*/
 
+	private void exposeIds(RepositoryRestConfiguration config) {
+
+		//OBTENGO UNA COLECCION DE TODAS LAS CLASES DE ENTIDAD DEL ADMINISTRADOR DE ENTIDADES
+		Set<EntityType<?>> entities=entityManager.getMetamodel().getEntities();
+		
+		//SE CREA UN ARRAY LIST PARA LOS TIPOS DE ENTIDAD
+		List<Class<?>> entityClasses=new ArrayList<>();
+		
+		//OBTENGO LOS TIPOS DE ENTIDAD PARA LAS ENTIDADES
+		for(EntityType<?> e:entities ) {
+			
+			entityClasses.add(e.getJavaType());
+		}
+		
+		//EXPONGO LOS IDS PARA LOS TIPOS DE ENTIDAD DE DOMINIO
+		Class<?>[] domainTypes=entityClasses.toArray(new Class[0]);
+		
+		config.exposeIdsFor(domainTypes);
+		
+	}
+	
+
+	//git pull	//BAJA LAS MODIFICACIONES  REMOTAS DE GIT HUB A TU REPOSITORIO LOCAL
+	// git push origin master	//SUBE TUS MODIFICACIONES LOCALES AL GIT HUB
+	
 }
